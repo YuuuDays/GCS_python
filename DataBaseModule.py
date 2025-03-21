7e586bb20f5f74d39fd2ff5a2f7dec61b02e7bad
hikida <ponponda0103@gmail.com>
2025-03-21 20:15:51 +0900

    リファクタリング

--- DataBaseModule.py
+++ DataBaseModule.py
@@ -5,35 +5,43 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
-def read_environmental_variables():
-    # .envファイル読み込み(ローカル用)
+# Cloud Run では `.env` を読まない
+if os.getenv("K_SERVICE") is None:
     load_dotenv()
-    # 環境変数から値を読み込み
+
+
+def read_environmental_variables():
+    """環境変数を読み取って MongoDB の URI を作成"""
     username = os.environ.get("NAME")
     password = os.environ.get("PASSWORD")
+    url_first = os.environ.get("URL_FIRST")
+    url_second = os.environ.get("URL_SECOND")
+
+    if not all([username, password, url_first, url_second]):
+        raise ValueError("環境変数が正しく設定されていません")
 
     # ユーザー名とパスワードをエンコード
     encoded_username = quote_plus(username)
     encoded_password = quote_plus(password)
 
     # 接続URIを構築
-    url_first = os.environ.get("URL_FIRST")
-    url_second = os.environ.get("URL_SECOND")
-
     uri = f"{url_first}{encoded_username}:{encoded_password}{url_second}"
-
+    print("★接続URI:", uri)
     return uri
 
 
 def db_call():
+    load_dotenv()
     uri = read_environmental_variables()
     # ↓DBサーバーにアクセスしているだけ
     client = MongoClient(uri, server_api=ServerApi('1'))
     # 指定された名前のdbにアクセス
     # 存在しない場合、仮想dbを作成しdataが挿入された時点で物理的なdbが作成される
-    db = client['gitInfoContributes']
+    db_name = os.environ.get("DB_NAME")
+    db = client[db_name]
     # コレクション(テーブル)へのアクセス
-    collection = db['user_info']
+    table_name = os.environ.get("TABLE_NAME")
+    collection = db[table_name]
 
     try:
         # 以下はadminデータBSに対してping コマンドを使用
@@ -67,36 +75,3 @@ def db_read(collection):
 
     print("【DBの値正常読み取れました】")
     return documents_count, docs_list
-
-# CRUD操作 - 作成-
-# def db_create(collection):
-#     new_document = {
-#         "git_name": "TANAKA",
-#         "mail": "sekandonoberu@yahoo.co.jp"
-#     }
-#     another_document = {
-#         "git_name": "HIKIDA",
-#         "mail": "ponponda0103@gmail.com"
-#     }
-#
-#     insert_result = collection.insert_one(new_document,another_document)
-#     print("【dbへの値が正常作成】")
-#     print(f"document ID = {insert_result}")
-#
-
-
-# def db_update(collection):
-#     # 特定のドキュメントを更新
-#     query = {"name": "TANAKA TAROU"}
-#     new_values = {"$set": {"name": "HIKIDA YUU"}}
-#
-#     update_result = collection.update_one(query, new_values)
-#     print(f"Matched documents: {update_result.matched_count}")
-#     print(f"Modified documents: {update_result.modified_count}")
-
-
-# def db_delete(collection):
-#     # 特定のドキュメントを削除
-#     query = {"name": "HIKIDA YUU"}
-#     delete_result = collection.delete_one(query)
-#     print(f"Deleted documents: {delete_result.deleted_count}")
