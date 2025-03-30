ebed094b0c602b9dbbb4ba724f5f18a24671d82f
hikida <ponponda0103@gmail.com>
2025-03-30 18:12:23 +0900

    宛先の本日の日付を追加

--- EmailContentTemplate.py
+++ EmailContentTemplate.py
@@ -1,6 +1,8 @@
 # 概要:メール内容のテンプレート作成
 import os
 import random
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 from dotenv import load_dotenv
 
@@ -8,7 +10,7 @@ if os.getenv("K_SERVICE") is None:
     load_dotenv()
 
 # メール構成内容テンプレート
-def define_body():
+def define_body(user_name):
     # ランダム要素とか持たせたいなぁ
     # 環境変数の取得
     frontend_path = os.getenv("FRONTEND_PATH")
@@ -17,10 +19,11 @@ def define_body():
         "今日のコミット頑張りましょう!!",
         "プッシュを忘れずに！",
         "小さな一歩が大きな成果に！",
-        "コードを書いたらこまめに保存しよう！"
-        "千里の道も一歩から！！！"
+        "コードを書いたらこまめに保存しよう！",
+        "千里の道も一歩から！！！今日から始めよう！"
     ]
     random_message = random.choice(messages)
+
 
     # メールの本文を作成
     body = (
@@ -32,5 +35,7 @@ def define_body():
     return body
 
 def define_subject():
-    subject = '本日のあなたのGitContributeについて'
+    now_japan = datetime.now(ZoneInfo("Asia/Tokyo"))  # 日本時間の取得
+    convert_japan = now_japan.strftime('%m/%d')  # MM/DD 形式の文字列に変換
+    subject = f"本日({convert_japan})のあなたのGitContributeについて"
     return subject
