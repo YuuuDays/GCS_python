9b66c0b050a7f22ef225b4c791d51af8794259db
hikida <ponponda0103@gmail.com>
2025-04-26 00:04:38 +0900

    テストコード追加

--- test_GetContributes.py
+++ test_GetContributes.py
@@ -2,7 +2,7 @@ from GetContributes import get_contribute_main, fetch_github_repos
 
 
 def test_should_send_mail():
-    document = [{"git_name":"YuuuDays","time":"23:45","mail":"sample@com"}]
+    document = [{"git_name":"YuuuDays","time":"00:00","mail":"sample@com"}]
     assert get_contribute_main(document)
 
 
